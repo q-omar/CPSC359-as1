1249b6900d008531bb36638c9b0e3ddfc0306134
simratv <32748426+simratv@users.noreply.github.com>
2018-06-09 22:55:05 +0000

    Update main.s

--- code/source/main.s
+++ code/source/main.s
@@ -266,7 +266,7 @@ leftmov:
 end1:
 	pop	{r4, r5, r6, r7, r8, pc}
 launchBall:
-	
+
 	push {r5, r6, r7, lr}
 	
 	ldr	r0, =ball	@ r0 = base address of ball
@@ -275,9 +275,9 @@ launchBall:
 	ldr	r0, =ball	@ r0 = base address of ball	
 	
 	@ Increment y- coordinate whenever B is pressed
-	ldr	r9, [r0, #4]	@ load y- coordinate of the ball
-	sub	r9, #1
-	str	r9, [r0, #4]
+	ldr	r6, [r0, #4]	@ load y- coordinate of the ball
+	sub	r6, #1
+	str	r6, [r0, #4]
 	
 	@ Check if the paddle is already at the right boundary	
 	ldr	r5, [r0]
@@ -288,11 +288,21 @@ launchBall:
 	@ If the balls's right end is not at the boundary, move right
 	cmp	r7, r6		
 	addlt	r5, #1
-	strlt	r5, [r0]
-	bl	drawImage	@ Redraw the moved ball
+	str	r5, [r0]
+	bllt	drawImage	@ Redraw the moved ball
 
+	mov	r0, #10000		// Slight delay while progressing the ball
+	bl	delayMicroseconds
+	
+	@	check for right bound
+	ldr	r0, =ball	@ r0 = base address of ball	
+	mov	r6, #rightBound
+	ldr	r7, [r0, #8]	@ r7 = width of ball
+	ldr	r5, [r0]
+	add	r7, r7, r5	@ r7 = x coordinate of right end of ball
 	cmp	r7, r6
-	bge	launchBall
+	blt	launchBall
+end2:
 	pop {r5, r6, r7, pc}
 
 @ Sets the health of all the bricks to full.
